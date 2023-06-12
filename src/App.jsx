532972adbd2cfd5d2a2456f9dd03df6c02dea18b
Mohammed Sanaved <mosanaved@gmail.com>
2023-06-13 01:30:29 +0530

    hero section processing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './components/Hero';
+import { heroapi } from './data/data';
+
+function App() {
+  return (
+    <>
+      <main>
+        <Hero heroapi={heroapi} />
+      </main>
+    </>
+  );
+}
+
+export default App;
